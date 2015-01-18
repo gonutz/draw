96040fce2f7847ad651690286ef9a7cc44f282cf
me <lars.scheme@gmail.com>
2015-01-18 23:10:50 +0100

    Added panel showing fore- and background colors.

---
+++ src/draw/CurrentColors.java
@@ -0,0 +1,39 @@
+package draw;
+
+import java.awt.Dimension;
+
+import javax.swing.JPanel;
+import java.awt.Color;
+import javax.swing.border.BevelBorder;
+
+public class CurrentColors extends JPanel implements CurrentColorsView {
+
+	private JPanel foreground;
+	private JPanel background;
+
+	public CurrentColors() {
+		setBorder(new BevelBorder(BevelBorder.LOWERED, null, null, null, null));
+		setPreferredSize(new Dimension(61, 61));
+		setLayout(null);
+
+		foreground = new JPanel();
+		foreground.setBackground(Color.BLACK);
+		foreground.setBounds(7, 7, 30, 30);
+		add(foreground);
+
+		background = new JPanel();
+		background.setBackground(Color.WHITE);
+		background.setPreferredSize(new Dimension(30, 30));
+		background.setBounds(23, 23, 30, 30);
+		add(background);
+	}
+
+	public void SetBackgroundColor(Color color) {
+		background.setBackground(color);
+		foreground.invalidate();
+	}
+
+	public void SetForegroundColor(Color color) {
+		foreground.setBackground(color);
+	}
+}
---
+++ src/draw/CurrentColorsView.java
@@ -0,0 +1,11 @@
+package draw;
+
+import java.awt.Color;
+
+public interface CurrentColorsView {
+
+	void SetBackgroundColor(Color color);
+
+	void SetForegroundColor(Color color);
+
+}
