ba3be91eafd292b2e9d2a39da9fc6f0bb2581880
Lars <lars.scheme@gmail.com>
2015-01-22 11:38:59 +0100

    Added overwrite prompt to 'save as' dialog.

--- src/draw/SwingSaveFileDialog.java
+++ src/draw/SwingSaveFileDialog.java
@@ -1,6 +1,10 @@
 package draw;
 
+import java.awt.HeadlessException;
+import java.io.File;
+
 import javax.swing.JFileChooser;
+import javax.swing.JOptionPane;
 import javax.swing.filechooser.FileFilter;
 import javax.swing.filechooser.FileNameExtensionFilter;
 
@@ -14,11 +18,28 @@ public class SwingSaveFileDialog implements SaveFileDialog {
 				"Portable Network Graphics (PNG)", "png");
 		chooser.setAcceptAllFileFilterUsed(false);
 		chooser.setFileFilter(imageFilter);
+		chooser.setMultiSelectionEnabled(false);
 	}
 
 	@Override
 	public boolean askUserForFileName() {
-		return chooser.showSaveDialog(null) == JFileChooser.APPROVE_OPTION;
+		boolean accepted = chooser.showSaveDialog(null) == JFileChooser.APPROVE_OPTION;
+		if (accepted && fileExists(getFileName()))
+			accepted = askIfUserWantsToOverwriteFile();
+		return accepted;
+	}
+
+	private boolean fileExists(String fileName) {
+		return new File(fileName).exists();
+	}
+
+	private boolean askIfUserWantsToOverwriteFile() {
+		final String message = "The file already exists. Do you want to overwrite it?";
+		try {
+			return JOptionPane.showConfirmDialog(null, message) == JOptionPane.OK_OPTION;
+		} catch (HeadlessException e) {
+			return true;
+		}
 	}
 
 	@Override
